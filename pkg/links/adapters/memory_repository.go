3b22f5397d61dec4dc5fea35fbaa01be6a76d5c3
Andrei Barbu <and.barbu@gmail.com>
2021-08-22 17:38:10 +1000

    add in-memory repository

--- pkg/links/adapters/memory_repository.go
+++ pkg/links/adapters/memory_repository.go
@@ -3,7 +3,6 @@ package adapters
 import (
 	"fmt"
 	"github.com/AndreiBarbuOz/lnkshrtn/pkg/links/domain"
-	"github.com/pkg/errors"
 	"sync"
 	"time"
 )
@@ -14,26 +13,37 @@ type MemoryLinkRepository struct {
 }
 
 func (r *MemoryLinkRepository) GetLinks() []*domain.Link {
-	panic("implement me")
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	ret := make([]*domain.Link, 0)
+	for _, value := range r.links {
+		ret = append(ret, &value)
+	}
+	return ret
 }
 
 func (r *MemoryLinkRepository) GetLink(shortned string) (*domain.Link, error) {
 	if ret, ok := r.links[shortned]; ok {
 		return &ret, nil
 	}
-	return nil, errors.New(fmt.Sprintf("didn't find %s\n", shortned))
+	return nil, fmt.Errorf("didn't find %s\n", shortned)
 }
 
 func (r *MemoryLinkRepository) CreateLink(url string, shortned string) (*domain.Link, error) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
 
+	if url == "" || shortned == "" {
+		return nil, fmt.Errorf("cannot create link with empty url or link")
+	}
+
 	link := domain.Link{
 		Url:      url,
 		Shortned: shortned,
 		Created:  time.Now(),
 	}
-	r.links[url] = link
+	r.links[shortned] = link
 	return &link, nil
 }
 
--- pkg/links/adapters/memory_repository_test.go
+++ pkg/links/adapters/memory_repository_test.go
@@ -10,4 +10,45 @@ func TestMemoryLinkRepository_CreateLink(t *testing.T) {
 	l, err := repo.CreateLink("url", "shortned")
 	assert.Nil(t, err, "Failed to create new Link")
 	assert.NotNil(t, l, "Create new link returned nil object")
+
+	assert.Equal(t, "url", l.Url, "Wrong Link object values")
+}
+
+func TestMemoryLinkRepository_CreateLinkEmpty(t *testing.T) {
+	repo := NewMemoryLinkRepository()
+	_, err := repo.CreateLink("", "shortned")
+	assert.NotNil(t, err, "Failed to return error on empty url")
+	_, err = repo.CreateLink("url", "")
+	assert.NotNil(t, err, "Failed to return error on empty shortned")
+}
+
+func TestMemoryLinkRepository_GetLinksEmpty(t *testing.T) {
+	repo := NewMemoryLinkRepository()
+	links := repo.GetLinks()
+	assert.NotNil(t, links, "GetLinks returned empty object")
+	assert.Len(t, links, 0, "Links len not zero")
+}
+
+func TestMemoryLinkRepository_GetLinksOneElem(t *testing.T) {
+	repo := NewMemoryLinkRepository()
+	_, _ = repo.CreateLink("url", "shortned")
+	links := repo.GetLinks()
+	assert.NotNil(t, links, "GetLinks returned empty object")
+	assert.Len(t, links, 1, "Links len not equal to 1")
+	assert.Equal(t, "url", links[0].Url)
+	assert.Equal(t, "shortned", links[0].Shortned)
+}
+
+func TestMemoryLinkRepository_GetLink(t *testing.T) {
+	repo := NewMemoryLinkRepository()
+	_, err := repo.GetLink("shortned")
+	assert.NotNil(t, err, "Returned non nil error for non existing link")
+	_, _ = repo.CreateLink("url", "shortned")
+	l, err := repo.GetLink("shortned")
+
+	assert.Nil(t, err, "Failed to get link")
+	assert.NotNil(t, l, "GetLink returned nil link")
+
+	_, err = repo.GetLink("non_existing")
+	assert.NotNil(t, err, "Returned non nil error for non existing link")
 }
